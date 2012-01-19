b7e415d28dfb4d0295aaf69cb71ecdf3ed14520d
Kai Bouché <kai@bouche.at>
2012-01-19 09:41:35 +0100

    small changes

--- src/instantsoupdata.py
+++ src/instantsoupdata.py
@@ -134,6 +134,9 @@ class Client(QtCore.QObject):
         # (timer) - (which is a QTimer)
         self.servers_timers = {}
 
+        # mapping from (server_id, channel) to a list containing messages
+        self.channel_history = {}
+
         # mapping from (server_id) to c(hannel_id) to a list of (client_ids)
         # stores the membership of this and OTHER peers
         self.membership = {}
@@ -179,18 +182,6 @@ class Client(QtCore.QObject):
         tcp_socket.flush()
         self.handle_data(data, tcp_socket)
         tcp_socket.disconnected.connect(tcp_socket.deleteLater)
-        data_text = InstantSoupData.command.parse(data)
-        if data_text.startswith("SAY"):
-            try:
-                opt, author_id, message = data_text.split("\x00")
-                nickname = self.users[author_id]
-                adress = socket.peerAddress()
-                port = socket.peerPort()
-                #channel_id = socket.
-                #self.client_message_received.emit(nickname, message, channel_id)              
-            except IndexError:
-                print "Unknown Message Format"
-                       
 
 #
     # PROCESSING FUNCTIONS (INCOMING SERVER COMMANDOS)
@@ -199,7 +190,12 @@ class Client(QtCore.QObject):
         data = InstantSoupData.command.parse(command)
 
         if data.startswith("SAY"):
-            print "SAY"
+            try:
+                client_id = data.split("\x00")[1]
+                message = " ".join(data.split("\x00")[2:])
+                print (client_id, ' say ', message)
+            except IndexError:
+                print "Unknown Message Format"
             #self.handle_say_command(data, tcp_socket)
         elif data.startswith("JOIN"):
             self.handle_join_command(data, tcp_socket)
@@ -696,3 +692,36 @@ class Server(QtCore.QObject):
 
     def send_datagram(self, datagram):
         self.udp_socket.writeDatagram(datagram, group_address, broadcast_port)
+
+# search a dictionary for key or value
+# using named functions or a class
+# tested with Python25   by Ene Uran    01/19/2008
+
+
+def find_key(dic, val):
+    """return the key of dictionary dic given the value"""
+    return [k for k, v in dic.iteritems() if v == val][0]
+
+
+def find_value(dic, key):
+    """return the value of dictionary dic given the key"""
+    return dic[key]
+
+
+class Lookup(dict):
+
+    """
+    a dictionary which can lookup value by key, or keys by value
+    """
+
+    def __init__(self, items=[]):
+        """items can be a list of pair_lists or a dictionary"""
+        dict.__init__(self, items)
+
+    def get_key(self, value):
+        """find the key(s) as a list given a value"""
+        return [item[0] for item in self.items() if item[1] == value]
+
+    def get_value(self, key):
+        """find the value given a key"""
+        return self[key]
